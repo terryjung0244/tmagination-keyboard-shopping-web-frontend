a0bcb19c0c48ac854044f4ab3463a0b96d39dcf8
terryjung0244 <terryjung0244@gmail.com>
2024-01-24 21:21:56 -0500

    Created code for image upload to firebase

--- src/layout/pages/dashboard/CreateKeyboard.tsx
+++ src/layout/pages/dashboard/CreateKeyboard.tsx
@@ -3,22 +3,12 @@
 import React, { useState } from 'react';
 import FireBaseUpload from '../../../components/FireBaseUpload';
 import { getUuid } from '../../../util/uuid';
-
-interface IKeyboardInputStateType {
-  keyboardName: string;
-  keyboardDesc: string;
-  keyboardPrice: string;
-  keyboardDiscountRate: number;
-  keyboardStock: string;
-  keyboardFeatures: {
-    color: string;
-    switch: string;
-  };
-  keyboardImageUrl: string;
-}
+import { IImageInfoStateType, IKeyboardInputStateType } from './CreateKeyboard.interface';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from '../../../firebase';
 
 const CreateKeyboard = () => {
-  const [imageInfo, setImageInfo] = useState<any>({
+  const [imageInfo, setImageInfo] = useState<IImageInfoStateType>({
     imageFile: null,
     imagePath: '',
   });
@@ -34,13 +24,24 @@ const CreateKeyboard = () => {
       switch: '',
     },
     keyboardImageUrl: '',
+    keyboardUploadImage: null, // 새로 만든거
   });
 
   const handleCreateKeyboard = async () => {
-    console.log(createKeyboardInput);
-    console.log(imageInfo);
-
     // 1. image upload to firebase storage
+    let uploadedImageUrl = '';
+    try {
+      const imageRef = ref(storage, imageInfo.imagePath);
+      const uploadResponse = await uploadBytes(imageRef, createKeyboardInput.keyboardUploadImage);
+      console.log('11');
+      if (uploadResponse) {
+        uploadedImageUrl = await getDownloadURL(uploadResponse.ref);
+        console.log('Successfully uploaded image');
+      }
+    } catch (err) {
+      alert(JSON.stringify(err));
+    }
+
     // 2. get image url
     // 3. validation
     // 4. backend로 보내기 (input + imageurl)
@@ -87,6 +88,7 @@ const CreateKeyboard = () => {
   return (
     <div>
       <button onClick={handleCreateKeyboard}>Create Keyboard</button>
+      <br />
       <input
         name="keyboardName"
         value={createKeyboardInput.keyboardName}
@@ -111,16 +113,16 @@ const CreateKeyboard = () => {
         onChange={handleKeyboardInput}
         placeholder="keyboardStock"
       />
+      <br />
       <select name={'color'} onChange={handleKeyboardSelectFeature}>
-        <option value={''} selected disabled hidden>
-          Select
-        </option>
+        <option value={'default'}>Keyboard Colors</option>
         <option>White</option>
         <option>Black</option>
         <option>Green</option>
         <option>Silver</option>
       </select>
       <select name={'switch'} onChange={handleKeyboardSelectFeature}>
+        <option value={'default'}>Switches</option>
         <option>Red Switch</option>
         <option>Black Switch</option>
         <option>Yellow Switch</option>
